0b26681a03bd70c56c1a7d4fbe220b1e623e08a2
Mausham <mausham.kafle@griffithuni.edu.au>
2023-09-05 20:19:00 +1000

    Added basic layout on the component pages and main page.

--- chat-system/src/app/app-routing.module.ts
+++ chat-system/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { ChatComponent } from './chat/chat.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent}, // Redirect to the login page
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'chat', component: ChatComponent },
+  { path: 'profile', component: UserProfileComponent },
+  // Add more routes as needed
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- chat-system/src/app/app.component.ts
+++ chat-system/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalStorageService } from './local-storage.service';
-import { mockUsers, mockGroups, mockChannels } from './mock-data'; // Import mock data
+import { mockUsers, mockGroups, mockChannels } from './mock-data/mock-data'; // Import mock data
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
-  constructor(private localStorageService: LocalStorageService) {}
+  constructor(private localStorageService: LocalStorageService) {};
+  title = 'Chat-System';
 
   ngOnInit(): void {
     // Check if mock data is already stored, if not, store it
