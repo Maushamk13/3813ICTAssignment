21181bd5f4d6d4f26b0c3fc29de4f91c7bf96f40
Mausham <mausham.kafle@griffithuni.edu.au>
2023-09-05 19:19:10 +1000

    Created components: registration, login and chat.

--- chat-system/src/app/app.module.ts
+++ chat-system/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    RegistrationComponent,
+    LoginComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
