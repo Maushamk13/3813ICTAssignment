88841ac5bf417948bf97ac98b03a35bf4dda2cea
Mausham <mausham.kafle@griffithuni.edu.au>
2023-09-07 03:21:13 +1000

    Added mock chat-user function and page.

--- chat-system/src/app/chat-user/chat-user.component.ts
+++ chat-system/src/app/chat-user/chat-user.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service'; // Import the user service
 
 @Component({
   selector: 'app-chat-user',
   templateUrl: './chat-user.component.html',
-  styleUrls: ['./chat-user.component.css']
+  styleUrls: ['./chat-user.component.css'],
 })
 export class ChatUserComponent {
+  newUser: any = {}; // Replace 'any' with your User interface
 
+  constructor(private userService: UserService) {}
+
+  registerUser(): void {
+    // Implement user registration logic here
+    // Send the newUser object to your server or update your local data
+
+    // Example: Add the newUser to a local array of users
+   // this.userService.addUser(this.newUser);
+    
+    // Clear the form after registration
+    this.newUser = {};
+  }
 }
+
